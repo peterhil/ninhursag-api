faafb8d62517d873e10325804a09e9f6306a737f
Peter Hillerström <peter.hillerstrom@gmail.com>
2014-03-01 13:13:40 +0200

    Rename instance/setting.cfg into instance/settings.py

--- app/factory.py
+++ app/factory.py
@@ -41,7 +41,7 @@ def create_app(package_name, package_path, settings_override=None):
     app = Flask(package_name, instance_relative_config=True)
 
     app.config.from_object('app.settings')
-    app.config.from_pyfile('settings.cfg', silent=True)
+    app.config.from_pyfile('settings.py', silent=True)
     app.config.from_object(settings_override)
 
     register_blueprints(app, package_name, package_path)
