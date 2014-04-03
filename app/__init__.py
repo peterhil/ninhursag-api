a8bc5aa392246bb6f7065e93618b526142d397bf
Peter Hillerström <peter.hillerstrom@gmail.com>
2014-04-03 21:23:03 +0300

    Move create_app() out of frontend module and fix imports.

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from app.frontend import create_app
+from flask import render_template
 from werkzeug.contrib.fixers import ProxyFix
 
+from . import factory
+from . import assets
 
-app = create_app()
+
+def create_app(settings_override=None):
+    """Returns the application instance"""
+    app = factory.create_app(__name__, __path__, settings_override)
+
+    # Init assets
+    assets.init_app(app)
+
+    # Register custom error handlers
+    if not app.debug:
+        for e in [500, 404]:
+            app.errorhandler(e)(handle_error)
+
+    return app
 
 
-# http://www.onurguzel.com/how-to-run-flask-applications-with-nginx-using-gunicorn/
-app.wsgi_app = ProxyFix(app.wsgi_app)
+def handle_error(e):
+    return render_template('errors/%s.html' % e.code), e.code
 
 
 if __name__ == '__main__':
+    app = create_app()
+    # http://www.onurguzel.com/how-to-run-flask-applications-with-nginx-using-gunicorn/
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run()
