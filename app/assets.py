36f71f15b72d5821fd226568fbfb72ed3391fb1b
Peter Hillerström <peter.hillerstrom@edocker.com>
2014-12-30 18:33:44 +0200

    Use bower installed version of d3.js.

--- app/assets.py
+++ app/assets.py
@@ -49,6 +49,12 @@ js_vendor = Bundle(
     output='gen/vendor.min.js'
 )
 
+js_d3 = Bundle(
+    'vendor/d3/d3.js',
+    filters='uglifyjs',
+    output='gen/d3.min.js'
+)
+
 js_ie = Bundle(
     'vendor/es5-shim/es5-shim.js',
     filters='uglifyjs',
@@ -71,11 +77,11 @@ js_main = Bundle(
     output='gen/app.min.js'
 )
 
-
 def init_app(app):
     webassets = Environment(app)
     webassets.register('css_all', css_all)
     webassets.register('js_vendor', js_vendor)
+    webassets.register('js_d3', js_d3)
     webassets.register('js_ie', js_ie)
     webassets.register('js_main', js_main)
     webassets.manifest = 'cache' if not app.debug else False
