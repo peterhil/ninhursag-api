280c8cab37b4daa11afd8681527e4628a8f42872
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-04-05 02:19:55 +0300

    Clean up SelectMineral

--- app/static/js/components/SelectMineral.svelte
+++ app/static/js/components/SelectMineral.svelte
@@ -1,5 +1,4 @@
 <script>
-    import { isEmpty } from 'ramda'
     import { minerals } from '../stores/minerals'
 
     export let selectedMineral = 'Gold'
@@ -10,11 +9,11 @@
     <p>Loading...</p>
     {:then $minerals}
     <select id="mineral" bind:value={selectedMineral}>
-        {#each [...Object.entries($minerals)] as [mineral, src] (mineral)}
+        {#each [...Object.keys($minerals)] as mineral}
         <option value="{mineral}" selected="{mineral === selectedMineral}">{mineral}</option>
         {/each}
     </select>
     {:catch error}
-    <p class="text-error">Problem loading mineral data: { $minerals.error }</p>
+    <p class="text-error">Problem loading mineral data: { error }</p>
     {/await}
 </label>
