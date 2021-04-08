521075231546390689b42c593af2f7f025b1a9b2
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-04-08 13:39:38 +0300

    Put selected mineral into store and simplify selection tracking by binding

--- app/static/js/pages/Minerals.svelte
+++ app/static/js/pages/Minerals.svelte
@@ -1,31 +1,21 @@
 <script>
-    import { onDestroy, onMount } from 'svelte'
-
     import References from '../../../templates/_references.html'
     import Images from '../components/Images.svelte'
     import SelectFunction from '../components/SelectFunction.svelte'
     import SelectMineral from '../components/SelectMineral.svelte'
     import { messages } from '../lib/messaging'
+    import { mineral } from '../stores/mineral'
 
-    export let mineral = 'Gold'
-
-    function onSelectMineral (value) {
-        mineral = value
-        return false
-    }
-
-    onMount(() => {
-        messages.on('select:mineral', onSelectMineral)
-    })
+    mineral = 'Gold'
 </script>
 
 <div class="row">
     <div class="small-12 large-9 columns">
-        <h1>{mineral}</h1>
+        <h1>{$mineral}</h1>
 
         <div class="row">
             <div class="large-4 columns">
-                <SelectMineral selected="{mineral}" />
+                <SelectMineral bind:selected="{$mineral}" />
             </div>
             <div class="large-4 columns">
                 <SelectFunction />
@@ -49,7 +39,7 @@
     </div>
 
     <aside class="small-12 large-3 columns">
-        <Images {mineral} />
+        <Images mineral={$mineral} />
         <References />
     </aside>
 </div>
