624d94d0758dd41fb1458c3a16d1fa12aa343bab
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-04-09 11:47:00 +0300

    Do not unneccessarily emit message on mineral selection

--- app/static/js/components/SelectMineral.svelte
+++ app/static/js/components/SelectMineral.svelte
@@ -1,20 +1,14 @@
 <script>
-    import { messages } from '../lib/messaging'
     import { minerals } from '../stores/minerals'
 
     export let selected
-
-    function onSelection (event) {
-        messages.emit('select:mineral', event.target.value)
-        return false
-    }
 </script>
 
 <label for="mineral">Select resource
     {#await $minerals }
     <p>Loading...</p>
     {:then $minerals}
-    <select id="mineral" bind:value={selected} on:change={onSelection}>
+    <select id="mineral" bind:value={selected}>
         {#each [...Object.keys($minerals)] as mineral}
         <option value="{mineral}" selected="{mineral === selected}">{mineral}</option>
         {/each}
