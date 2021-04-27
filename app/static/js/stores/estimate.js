1df5048de1b1b11450653072ce60f4d3c43ce6d8
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-04-27 13:10:17 +0300

    Cancel requests on estimate store before making new ones

--- app/static/js/stores/estimate.js
+++ app/static/js/stores/estimate.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { CancelToken } from 'axios'
 import { values } from 'ramda'
 import { asyncable } from 'svelte-asyncable'
 
@@ -6,6 +7,8 @@ import { errorHandler } from '../lib/api'
 import { chartDataFromEstimate, dataForEstimate } from '../lib/estimate'
 import { fn } from './function.js'
 import { data } from './data.js'
+
+let cancel
 
 export const estimate = asyncable(async ($data, $fn) => {
     try {
@@ -15,15 +18,25 @@ export const estimate = asyncable(async ($data, $fn) => {
             'function': fn,
             ...dataForEstimate(values(data.data), data.selected, 'Year')
         }
-        // console.debug(`Estimating with ${fn}`, data, params)
+        // console.debug(`Estimating with ${fn}`, params)
 
-        const res = await axios.post('/api/v1/estimate', params)
+        if (cancel) { cancel() }
+
+        const res = await axios.post('/api/v1/estimate', params, {
+            cancelToken: new CancelToken(function executor(c) {
+                cancel = c
+            }),
+        })
         const estimated = chartDataFromEstimate(res.data, data.selected, fn)
         console.debug(`Estimated with ${fn}`, estimated)
 
         return estimated
     } catch (error) {
-        errorHandler(error)
-        return error
+        if (axios.isCancel(error)) {
+            console.log('Request canceled');
+        } else {
+            errorHandler(error)
+            return error
+        }
     }
 }, undefined, [data, fn])
