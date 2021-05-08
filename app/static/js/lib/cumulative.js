b5251efb21d9f0fb20374c829c324642970e85f3
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-05-08 23:35:58 +0300

    Remove accumulateData from lib/cumulative

--- app/static/js/lib/cumulative.js
+++ app/static/js/lib/cumulative.js
@@ -1,17 +1,4 @@
-import {
-    filter,
-    fromPairs,
-    defaultTo,
-    head,
-    indexBy,
-    is,
-    keys,
-    last,
-    map,
-    mapObjIndexed,
-    prop,
-    range,
-} from 'ramda'
+import { mapObjIndexed } from 'ramda'
 import { accumulator, finite } from './data'
 
 export function accumulateDataSeries (dataSeries) {
@@ -19,37 +6,4 @@ export function accumulateDataSeries (dataSeries) {
     const cumulative = mapObjIndexed(accumulator(), numbers)
 
     return cumulative
-}
-
-export function accumulateData (data, column, series, extraYears = 0) {
-    let currentYear
-    let total = 0
-
-    // Check if request was canceled and thus column is undefined
-    if (!column) {
-        console.assert(column, 'Empty column for:', series, data)
-        return data
-    }
-
-    const dataYears = filter((row) => is(Number, row[column]), data.data)
-    const first = parseInt(head(keys(dataYears)))
-    const latest = parseInt(last(keys(dataYears)))
-    const years = range(first, latest + extraYears + 1)
-
-    // console.debug('[AccumulateData] Range:', first, latest, column)
-
-    // TODO Write separate function that uses world production data until the estimation continues!
-    return indexBy(prop('Year'), map((year) => {
-        if (year <= latest) {
-            currentYear = defaultTo(0, parseFloat(data.data[year][column]))
-        } else {
-            currentYear = data.data[latest][column]
-        }
-        total += currentYear
-
-        return fromPairs([
-            ['Year', year],
-            [series, total],
-        ])
-    }, years))
 }
