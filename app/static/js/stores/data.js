05da618e618869df90d5147f441c4ada0916ba81
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-05-14 05:44:50 +0300

    eslint: Clean up data store

--- app/static/js/stores/data.js
+++ app/static/js/stores/data.js
@@ -4,31 +4,28 @@ import api from '../lib/api'
 import { cleanup } from '../lib/csv'
 import { alerts } from '../stores/alerts'
 import { mineral } from './mineral.js'
-import { minerals } from './minerals.js'
 
 function errorHandler (error) {
-    // console.error('Error happened:', error)
     alerts.error(error.message)
     throw error
 }
 
-export const data = asyncable(async ($mineral, $minerals) => {
-    const mineral = await $mineral
-    const minerals = await $minerals
-    if (!mineral) {
-        return []
-    }
-    const src = minerals[mineral]
-    // const url = `/static/data/tsv/${src}`
-    const url = encodeURI('/api/v1/mineral/' + mineral)
-    // console.debug('[Data] Fetching:', url)
+export const data = asyncable(
+    async ($mineral) => {
+        const mineral = await $mineral
 
-    const parsed = await api.get(
-        url,
-        (rawData) => cleanup(rawData),
-        errorHandler,
-    )
-    // console.debug('[Data] Store:', parsed)
+        const url = encodeURI('/api/v1/mineral/' + mineral)
+        // console.debug('[Data] Fetching:', url)
 
-    return parsed
-}, undefined, [mineral, minerals])
+        const parsed = await api.get(
+            url,
+            (rawData) => cleanup(rawData),
+            errorHandler,
+        )
+        // console.debug('[Data] Store:', parsed)
+
+        return parsed
+    },
+    undefined,
+    [mineral]
+)
