838177dddcc774a1d0e534ff24a8f409e505b371
Peter Hillerström <peter.hillerstrom@gmail.com>
2022-01-10 11:03:28 +0200

    Remove Angular scripts, templates and views

--- app/assets.py
+++ app/assets.py
@@ -19,26 +19,10 @@ css_foundation = Bundle(
 )
 
 css_all = Bundle(
-    'vendor/angular-growl-v2/build/angular-growl-foundation.css',
     css_foundation,
     css_application,
     filters='cssmin',
     output='gen/app.min.css'
-)
-
-js_angular = Bundle(
-    'vendor/angular/angular.js',
-    'vendor/angular-animate/angular-animate.js',
-    # 'vendor/angular-aria/angular-aria.js',
-    'vendor/angular-messages/angular-messages.js',
-    'vendor/angular-resource/angular-resource.js',
-    'vendor/angular-route/angular-route.js',
-    'vendor/angular-sanitize/angular-sanitize.js',
-    # 'vendor/angular-touch/angular-touch.js',
-    'vendor/angular-growl-v2/build/angular-growl.js',
-    'vendor/angular-spinner/dist/angular-spinner.js',
-    filters='rjsmin',
-    output='gen/angular.min.js',
 )
 
 js_foundation = Bundle(
@@ -63,32 +47,11 @@ js_vendor = Bundle(
     output='gen/vendor.min.js'
 )
 
-js_d3 = Bundle(
-    'vendor/d3/d3.js',
-    filters='uglifyjs',
-    output='gen/d3.min.js'
-)
-
-js_main = Bundle(
-    'script/app.coffee',
-    'script/service/config.coffee',
-    'script/service/api.coffee',
-    'script/service/functional.coffee',
-    'script/controller/mineral-ctrl.coffee',
-    'script/directive/chart.coffee',
-    'script/directive/resource.coffee',
-    filters=['coffeescript', 'rjsmin'],
-    output='gen/app.min.js'
-)
-
 def init_app(app):
     webassets = Environment(app)
     webassets.register('css_all', css_all)
-    webassets.register('js_angular', js_angular)
     webassets.register('js_foundation', js_foundation)
     webassets.register('js_vendor', js_vendor)
-    webassets.register('js_d3', js_d3)
-    webassets.register('js_main', js_main)
     webassets.manifest = 'cache' if not app.debug else False
     webassets.cache = not app.debug
     webassets.debug = app.debug
--- app/static/script/directive/resource.coffee
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-angular.module('app')
-  .directive 'resource', ['$log', 'api', ($log, api) ->
-    restrict: 'A'
-    replace: true
-    templateUrl: '/static/template/resource.html'
-  ]
