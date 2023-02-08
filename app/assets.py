f73e26b92c2048d7fe529d10dec3a908cb440725
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-09 01:21:43 +0200

    deps: Get rid of all bloated and obsolete shit like jQuery, Foundation, es5-shim and what-input

--- app/assets.py
+++ app/assets.py
@@ -4,42 +4,26 @@
 from flask_assets import Environment, Bundle
 
 
-css_application = Bundle(
+css_main = Bundle(
     'style/main.scss',
     debug=False,
     filters='scss',
     output='gen/app.css'
 )
 
-css_foundation = Bundle(
-    'vendor/foundation-sites/scss/normalize.scss',
-    'vendor/foundation-sites/scss/foundation.scss',
-    filters='scss',
-    output='gen/foundation.css'
-)
-
 css_all = Bundle(
-    css_foundation,
-    css_application,
+    css_main,
     filters='cssmin',
     output='gen/app.min.css'
 )
 
-js_foundation = Bundle(
-    'vendor/jquery/dist/jquery.js',
-    'vendor/js-cookie/src/js.cookie.js',
-    'vendor/foundation-sites/js/foundation/foundation.js',
-    'vendor/foundation-sites/js/foundation/foundation.topbar.js',
-    filters='rjsmin',
-    output='gen/foundation.min.js'
-)
-
 js_vendor = Bundle(
-    'vendor/spin.js/spin.js',
-    'vendor/papaparse/papaparse.js',
     'vendor/humanize-plus/public/src/humanize.js',
+    'vendor/js-cookie/src/js.cookie.js',
+    'vendor/papaparse/papaparse.js',
     'vendor/ramda/dist/ramda.js',
     'vendor/soundex-code/index.js',
+    'vendor/spin.js/spin.js',
     'vendor/tinycolor2/tinycolor.js',
     filters='rjsmin',
     output='gen/vendor.min.js'
@@ -48,7 +32,6 @@ js_vendor = Bundle(
 def init_app(app):
     webassets = Environment(app)
     webassets.register('css_all', css_all)
-    webassets.register('js_foundation', js_foundation)
     webassets.register('js_vendor', js_vendor)
     webassets.manifest = 'cache' if not app.debug else False
     webassets.cache = not app.debug
