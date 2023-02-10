2ac1b8276510100edadf6f3a35cc73b279ddd89d
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-10 07:21:58 +0200

    Remove extra columns classes

--- app/static/js/components/Notifications.svelte
+++ app/static/js/components/Notifications.svelte
@@ -3,7 +3,7 @@
     import { alerts } from '../stores/alerts'
 </script>
 
-<div id="notifications" class="columns">
+<div id="notifications">
     {#each [...$alerts.entries()] as [id, alert] (id) }
         <Alert type={alert.type} on:click={() => alerts.remove(id)}>
             {alert.message}
