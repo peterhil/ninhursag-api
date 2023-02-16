f289954508ecab5dca57d9e1ba8c57c3fa907e12
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-16 04:03:09 +0200

    Fix tabindex error

--- app/static/js/components/Alert.svelte
+++ app/static/js/components/Alert.svelte
@@ -6,11 +6,15 @@
 </script>
 
 <div class="alert-box {type}"
-     tabindex="0" aria-live="assertive" role="alertdialog"
+     tabindex="-1"
+     aria-live="assertive"
+     role="alertdialog"
      in:fade="{{ duration: 100 }}"
      out:fade="{{ duration: 500 }}">
     <slot>{message}</slot>
-    <button class="close" on:click
-            tabindex="0" aria-label="Close Alert"
+    <button class="close"
+            on:click
+            tabindex="0"
+            aria-label="Close Alert"
             >&times;</button>
 </div>
