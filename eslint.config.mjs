3c4fc2c5d6c294f8fd8753533eeff3d04cd942e5
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-04-20 23:39:35 +0300

    eslint: Update to v9 and migrate config to eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import svelte from 'eslint-plugin-svelte'
+
+import { fileURLToPath } from 'node:url'
+import { includeIgnoreFile } from '@eslint/compat'
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    includeIgnoreFile(gitignorePath),
+    js.configs.recommended,
+    ...svelte.configs['flat/recommended'],
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+        rules: {
+            'comma-dangle': ['off', 'always'],
+            'indent': ['error', 4],
+            'no-console': ['off', 'always'],
+        },
+    },
+]
