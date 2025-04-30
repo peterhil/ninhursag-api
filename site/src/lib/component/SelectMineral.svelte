05f436bbb81b2f06451abcbc0f85a09bce6e5f1f
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-04-30 14:28:47 +0300

    Implement changing document title and location with pushState when a mineral is selected

--- site/src/lib/component/SelectMineral.svelte
+++ site/src/lib/component/SelectMineral.svelte
@@ -2,6 +2,8 @@
 	import debounce from 'debounce'
 	import { HeavyMetal } from '@icon-park/svg'
 
+	import { pushState } from '$app/navigation'
+
 	import IconPark from '$component/IconPark.svelte'
 
 	const title = "Resource"
@@ -11,7 +13,13 @@
 	const onSelected = (event) => {
 	    selected = event.target.value
 	    console.info('Mineral:', selected)
-	    return false
+
+		pushState('/mineral/' + selected, {
+			mineral : selected,
+		})
+		document.title = `${selected} statistics | Ninhursag`
+
+		return false
 	}
 
 	const onSelectedDebounced = debounce(onSelected, 500)
---
+++ site/src/routes/mineral/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+	import type { MineralsOutput } from '$lib/types'
+
+	import Minerals from '$component/Minerals.svelte'
+	import { mineral } from '$store/mineral'
+
+	type OutputProps = PageData & MineralsOutput
+
+	interface Props {
+		data: OutputProps;
+	}
+
+	let { data }: Props = $props();
+</script>
+
+<svelte:head>
+	<title>{data.mineral} statistics | Ninhursag</title>
+</svelte:head>
+
+<main>
+	<Minerals {data}/>
+</main>
